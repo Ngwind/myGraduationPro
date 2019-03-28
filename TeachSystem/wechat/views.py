0c4a8c44f44a44e3b42ea1aa89418468217835f9
Ngwind <137476684@qq.com>
2019-03-29 01:17:21 +0800

    字符串进行编码

--- TeachSystem/wechat/views.py
+++ TeachSystem/wechat/views.py
@@ -14,7 +14,7 @@ def we_chat_main(request):
         # 把参数放到list中排序后合成一个字符串，再用sha1加密得到新的字符串与微信发来的signature对比，如果相同就返回echostr给服务器，校验通过
         hash_list = [token, timestamp, nonce]
         hash_list.sort()
-        hash_str = ''.join([s for s in hash_list])
+        hash_str = ''.join([s for s in hash_list]).encode("utf-8")
         hash_str = hashlib.sha1(hash_str).hexdigest()
         if hash_str == signature:
             return HttpResponse(echo_str)
