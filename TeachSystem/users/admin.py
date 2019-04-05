b6111bfb50fed74fab859034b163bb3845fdc3be
wenda <41123748+Ngwind@users.noreply.github.com>
2019-04-05 23:34:31 +0800

    1. 因为teacherAdmin继承自useradmin，不能直接改成importexportmodeladmin。所以用importexportmixin的方式，多父类继承实现。 2. 发现并修复了之前的一个bug。自定义user模型为teacher之后，admin后台注册界面并不会出现自定义字段'college'。需要重写get_fieldsets函数来重写fieldsets(往里面增加一个'college'字段). 3. 实现学生和教师的导出功能。通过重写__init__和get_export_fields函数，实现将导出的field的column名修改为verbose_name（本来直接是数据库里面的英文字段名）。

--- TeachSystem/users/admin.py
+++ TeachSystem/users/admin.py
@@ -1,12 +1,32 @@
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 from import_export import resources
+from django.apps import apps
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Teacher, Student
-from courses.models import CourseProgress, Video # 忽略这些红色波浪线，其实是正确的
+from courses.models import CourseProgress, Video  # 忽略这些红色波浪线，其实是正确的
 
 
 class StudentResource(resources.ModelResource):
+    """学生导入导出文件配置"""
+
+    def __init__(self):
+        super(StudentResource, self).__init__()
+        # 获取所有字段的verbose_name并存放在字典
+        field_list = apps.get_model('users.Student')._meta.fields
+        self.vname_dict = {}
+        for i in field_list:
+            self.vname_dict[i.name] = i.verbose_name
+
+    def get_export_fields(self):
+        """默认导入导出field的column_name为字段的名称，这里修改为字段的verbose_name"""
+        fields = self.get_fields()
+        for field in fields:
+            field_name = self.get_field_name(field)
+            # 如果我们设置过verbose_name，则将column_name替换为verbose_name。否则维持原有的字段名
+            if field_name in self.vname_dict.keys():
+                field.column_name = self.vname_dict[field_name]
+        return fields
 
     class Meta:
         model = Student
@@ -18,6 +38,7 @@ class StudentResource(resources.ModelResource):
 
 @admin.register(Student)
 class StudentAdmin(ImportExportModelAdmin):
+    """学生admin后台配置"""
     list_display = ('studentId', 'username', 'college', 'userclass', 'createdate')
     search_fields = ['studentId', 'username', 'college', 'userclass']
     list_filter = ['college', 'userclass']
@@ -40,4 +61,49 @@ class StudentAdmin(ImportExportModelAdmin):
         return self.readonly_fields
 
 
-admin.site.register(Teacher, UserAdmin)
+class TeacherResource(resources.ModelResource):
+    """教师导入导出文件配置"""
+    def __init__(self):
+        super(TeacherResource, self).__init__()
+        # 获取所有字段的verbose_name并存放在字典
+        field_list = apps.get_model('users.Teacher')._meta.fields
+        self.vname_dict = {}
+        for i in field_list:
+            self.vname_dict[i.name] = i.verbose_name
+
+    def get_export_fields(self):
+        """默认导入导出field的column_name为字段的名称，这里修改为字段的verbose_name"""
+        fields = self.get_fields()
+        for field in fields:
+            field_name = self.get_field_name(field)
+            # 如果我们设置过verbose_name，则将column_name替换为verbose_name。否则维持原有的字段名
+            if field_name in self.vname_dict.keys():
+                field.column_name = self.vname_dict[field_name]
+        return fields
+
+    class Meta:
+        model = Teacher
+        fields = ('username', 'first_name', 'college',)
+        # import_id_fields = ('id',)
+        export_order = ('username', 'first_name', 'college',)
+        skip_unchanged = True
+
+
+
+@admin.register(Teacher)
+class TeacherAdmin(ImportExportMixin, UserAdmin):
+    """教师admin配置"""
+    list_display = ['first_name', 'username', 'college', 'is_staff', 'date_joined']
+    ordering = ['first_name', 'username', 'college', 'is_staff', 'date_joined']
+    resource_class = TeacherResource
+
+    def get_fieldsets(self, request, obj=None):
+        """在教师信息界面中的'个人信息'中添加college字段"""
+        tp = super(UserAdmin, self).get_fieldsets(request, obj)
+        ls = list(tp)
+        new_list = list(ls[1][1]['fields'])
+        if 'college' not in new_list:
+            new_list.append('college')
+        new_tp = tuple(new_list)
+        ls[1][1]['fields'] = new_tp
+        return tuple(ls)
