8dd15fcc21e9250f09278e34b448318343e7ea84
wenda <41123748+Ngwind@users.noreply.github.com>
2019-04-10 11:44:46 +0800

    测试get

--- TeachSystem/users/views.py
+++ TeachSystem/users/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(request.path)
+    openid = request.GET.get('openid', default=None)
+    return HttpResponse(request.path+openid)
