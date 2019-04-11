714f011a3c82d0cca3d4fc4c615e90b2c8ab1727
wenda <41123748+Ngwind@users.noreply.github.com>
2019-04-11 20:38:51 +0800

    增加login页面

--- TeachSystem/users/views.py
+++ TeachSystem/users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 
 def index(request):
 
     return HttpResponse(request.path)
+
+
+def login(request):
+    """返回静态登录页面"""
+    return render(request, "login.html", context={})
+
+
+def check_login(request):
+    """接受一个表单，校验登录"""
+    pass
+
