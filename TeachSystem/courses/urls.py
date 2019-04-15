da685d776e6b761174a861628c900bd00074941e
wenda <41123748+Ngwind@users.noreply.github.com>
2019-04-15 23:46:30 +0800

    优化video展示页面。没有视频时弹窗提示，用户确认后回退到课程展示页。

--- TeachSystem/courses/urls.py
+++ TeachSystem/courses/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("courselist/", views.re_course_list, name="courselist"),
-    path("learning/", views.re_video_list, name="learning"),
+    path("learning/", views.index, name="learning"),
 ]
